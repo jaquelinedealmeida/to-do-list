962922bf7092b3e270edb5753e02a0dad7e6d7af
Jaqueline de Almeida <jaqueshanti@gmail.com>
2025-01-21 18:42:34 -0300

    chore: fix file server and scripts

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-import { create, router as _router, defaults } from 'json-server';
-const server = create();
+import express from 'express';
+import { join } from 'path';
+import { router as _router, defaults } from 'json-server';
+const { static: serveStatic } = express;
+
+const app = express();
 const router = _router('db.json');
 const middlewares = defaults();
 
 const PORT = process.env.PORT || 5000;
 
-server.use(middlewares);
-server.use(router);
+app.use(serveStatic(join(__dirname, 'build')));
 
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+app.use('/api', middlewares, router);
+
+
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
