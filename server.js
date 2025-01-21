35ab2e9421a64b0c6fb8e0ac5ab5f9de6920ce18
Jaqueline de Almeida <jaqueshanti@gmail.com>
2025-01-21 18:28:22 -0300

    chore: adds file server

---
+++ server.js
@@ -0,0 +1,13 @@
+import { create, router as _router, defaults } from 'json-server';
+const server = create();
+const router = _router('db.json');
+const middlewares = defaults();
+
+const PORT = process.env.PORT || 5000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
